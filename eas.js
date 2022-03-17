2a0b7a7a8d57c7aa0e5b1e095521cc0e56a2d5f2
Joshua Williams <j.williams@linuxmail.org>
2022-03-16 23:23:33 -0400

    added etchasketch

---
+++ eas.js
@@ -0,0 +1,31 @@
+function addEvent(elm)
+{
+    elm.addEventListener('mouseover', (e) => 
+    {
+        let red = Math.floor(Math.random() * 250 + 0);
+        let green = Math.floor(Math.random() * 250 + 0);
+        let blue = Math.floor(Math.random() * 250 + 0);
+
+        e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    });
+}
+
+// Create a grid element
+const grid = document.createElement('div');
+grid.classList.add('grid');
+
+document.body.appendChild(grid);
+
+
+for (i = 0; i < 30000; i++)
+{
+    let square = document.createElement('div');
+    square.classList.add('square');
+    grid.appendChild(square);
+
+    addEvent(square);
+}
+
+
+
+
