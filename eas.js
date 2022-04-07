0ae0182f7b9bfe7781af1b82e25c351650529296
Joshua Williams <j.williams@linuxmail.org>
2022-04-06 23:10:53 -0400

    Doodle update!

--- eas.js
+++ eas.js
@@ -24,6 +24,58 @@ pRainbow.addEventListener('click', (e) =>
 });
 
 
+function saveState()
+{
+    console.log("saving doodle...");
+    let lastDrawing = {};
+
+    for(let i = 1; i <= 7100; i++)
+    {
+        let classes = document.getElementById(i).classList;
+        lastDrawing[i] = classes;
+    }
+
+    localStorage.lastDrawing = JSON.stringify(lastDrawing);
+    console.log("saved doodle!");
+}
+
+
+async function loadState()
+{
+    console.log("loading doodle...");
+    let lastDrawing = JSON.parse(localStorage.lastDrawing) ?? {};
+
+    for(let i = 1; i <= 7100; i++)
+    {
+        document.getElementById(i).classList.remove("square");
+        document.getElementById(i).classList.remove("rainbow");
+        document.getElementById(i).classList.remove("flashy");
+
+        if (lastDrawing[i][1] != 'boxy')
+        {
+            document.getElementById(i).classList.add(lastDrawing[i][1]);
+            await new Promise(r => setTimeout(r, 2));
+        }
+        else
+        {
+            document.getElementById(i).classList.add("square");
+            document.getElementById(i).classList.add('boxy'); 
+        }
+    }
+    console.log("loaded doodle!");
+}
+
+function resetState()
+{
+
+    for(let i = 1; i <= 7100; i++)
+    {
+        document.getElementById(i).classList = {};
+        document.getElementById(i).classList.add('square');
+        document.getElementById(i).classList.add('boxy');
+    }
+}
+
 function addEvent(elm)
 {
     elm.addEventListener('pointerover', (e) => 
@@ -42,17 +94,27 @@ function addEvent(elm)
 const grid = document.createElement('div');
 grid.classList.add('grid');
 
-document.body.appendChild(grid);
+document.getElementById("gridtainer").appendChild(grid);
 
 
-for (i = 0; i < 20000; i++)
+for (i = 0; i <= 70; i++)
 {
-    let square = document.createElement('div');
-    square.classList.add('square');
-    square.classList.add('boxy');
-    grid.appendChild(square);
+    let pegRow = document.createElement('div');
+    pegRow.classList.add('pegRow');
+    
+    for(let peg = 1; peg <= 100; peg++)
+    {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.classList.add('boxy');
+        square.id = (i * 100) + peg;
 
-    addEvent(square);
+        pegRow.appendChild(square);
+    
+        addEvent(square);
+    }
+
+    grid.appendChild(pegRow);
 }
 
 
