0e0d3e86171a026c7ba1df4efdcb3e777bcf7ef4
Joshua Williams <j.williams@linuxmail.org>
2022-04-07 13:12:35 -0400

    Corrected issue where brushes were not rendering properly.

--- eas.js
+++ eas.js
@@ -1,6 +1,14 @@
 let mouseDown = 0;  
 let paletteChoice = "flashy";
+let isResetting = false;
+let isLoading = false;
+let isSaving = false;
 const pLoad = document.getElementById("load");
+
+const pUnicorn = document.getElementById("nunicorn");
+const pRainbow = document.getElementById("nainbow");
+const pEraser = document.getElementById("eraser");
+
 
 window.onpointerdown = () => 
 {  
@@ -11,13 +19,6 @@ window.onpointerup = () => {
   mouseDown = 0;  
 }
 
-/*window.ondragstart = () => {
-    mouseDown = 0;  
-}*/
-
-const pUnicorn = document.getElementById("nunicorn");
-const pRainbow = document.getElementById("nainbow");
-const pEraser = document.getElementById("eraser");
 
 pUnicorn.addEventListener('click', (e) =>
 {
@@ -31,60 +32,95 @@ pRainbow.addEventListener('click', (e) =>
 
 pEraser.addEventListener('click', (e) =>
 {
-    paletteChoice = "square";
+    paletteChoice = "black";
 })
 
 
 function saveState()
 {
+    if (isResetting || isLoading) { return false; }
+
+    isSaving = true;
+
+    enableDisableButtons(false);
     console.log("saving doodle...");
     let lastDrawing = {};
 
     for(let i = 1; i <= 7100; i++)
     {
-        let classes = document.getElementById(i).classList;
+        let classes = {...document.getElementById(i).classList};
         lastDrawing[i] = classes;
     }
 
     localStorage.lastDrawing = JSON.stringify(lastDrawing);
+
+    isSaving = false;
+    enableDisableButtons(true);
     console.log("saved doodle!");
 }
 
 
 async function loadState()
 {
+    if (isSaving || isResetting) { console.log("can't load, saving."); return false;}
+
+    isLoading = true;
+
+    enableDisableButtons(false);
+
     console.log("loading doodle...");
     let lastDrawing = JSON.parse(localStorage.lastDrawing) ?? {};
 
     for(let i = 1; i <= 7100; i++)
     {
-        document.getElementById(i).classList.remove("square");
-        document.getElementById(i).classList.remove("rainbow");
-        document.getElementById(i).classList.remove("flashy");
+        var classList = document.getElementById(i).classList;
 
-        if (lastDrawing[i][1] != 'boxy')
-        {
-            document.getElementById(i).classList.add(lastDrawing[i][1]);
-            await new Promise(r => setTimeout(r, 2));
-        }
-        else
-        {
-            document.getElementById(i).classList.add("square");
-            document.getElementById(i).classList.add('boxy'); 
-        }
+        classList.remove(classList.item(1));
+        classList.add(lastDrawing[i][1]);
     }
+
+    isLoading = false;
+    enableDisableButtons(true);
     console.log("loaded doodle!");
+}
+
+function enableDisableButtons(enable)
+{
+    if (enable)
+    {
+        document.getElementById("reset").disabled = false;
+        document.getElementById("save").disabled = false;
+        document.getElementById("load").disabled = false;
+    }
+    else
+    {
+        document.getElementById("reset").disabled = true;
+        document.getElementById("save").disabled = true;
+        document.getElementById("load").disabled = true;
+    }
 }
 
 function resetState()
 {
+    if (isLoading || isSaving) { return false;}
+
+    enableDisableButtons(false);
+
+    isResetting = true;
 
     for(let i = 1; i <= 7100; i++)
     {
-        document.getElementById(i).classList = {};
-        document.getElementById(i).classList.add('square');
-        document.getElementById(i).classList.add('boxy');
+        var classList = document.getElementById(i).classList;
+
+        classList.remove(classList.item(1));
+        classList.remove(classList.item(0));
+
+        classList.add('boxy');
+        classList.add('black');
     }
+
+    enableDisableButtons(true);
+    isResetting = false;
 }
 
 function addEvent(elm)
@@ -93,7 +129,7 @@ function addEvent(elm)
     {
         if (mouseDown > 0)
         {
-            e.target.classList.remove("square");
+            e.target.classList.remove("black");
             e.target.classList.remove("rainbow");
             e.target.classList.remove("flashy");
             e.target.classList.add(paletteChoice);
@@ -118,8 +154,8 @@ for (i = 0; i <= 70; i++)
     for(let peg = 1; peg <= 100; peg++)
     {
         let square = document.createElement('div');
-        square.classList.add('square');
         square.classList.add('boxy');
+        square.classList.add('black');
         square.draggable = false;
         square.id = (i * 100) + peg;
 
